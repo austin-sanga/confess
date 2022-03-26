05e143c42c13b18866852214bd23c303fe30d3a0
austin-sanga <austinsanga2000@gmail.com>
2022-03-26 23:19:46 +0300

    first migrate

---
+++ database/migrations/2022_03_26_195312_create_confessions_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateConfessionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('confessions', function (Blueprint $table) {
+            $table->increaments("id");
+            $table->string('confession');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('confessions');
+    }
+}
---
+++ resources/views/hero.blade.php
@@ -0,0 +1,7 @@
+{{-- This is the first page a user lands into and confess their thoughts --}}
+
+<h1>Hero page</h1>
+<form action="" method="" >
+    <input type="text" name="confession" placeholder="Confess here.....">
+    <button type="submit">publish</button>
+</form>

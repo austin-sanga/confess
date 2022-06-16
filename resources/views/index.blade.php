7011a24dceb3fe0e492c9f25c89a4b19b95aa84f
austin-sanga <austinsanga2000@gmail.com>
2022-06-16 10:12:00 +0300

    master layout

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,14 +1,19 @@
 {{-- This is the first page a user lands into and confess their thoughts --}}
 
-<h1>Whats on your mind</h1>
+{{-- extends the master layout file --}}
+@extends('master')
+
+@section('content')
+
+<h1 class=" text-center">Whats on your mind ?</h1>
 
 {{-- so here is how a session is implied to send verification to user that data is uploaded --}}
 @if (session('confession'))
-    <h3 style="color: green">confession received</h3>
+    <h3 style="color: green" class=" text-center">confession received</h3>
 @endif
 
 {{-- span to show the error message on blank publishing --}}
-<span style="color: red">
+<span style="color: red" class=" text-center">
 
     {{-- this is how you display the validation error --}}
     @error('confession')
@@ -16,8 +21,11 @@
     @enderror
 
 {{-- the action comes from the route of the controller for the form to pass through --}}
-<form action="published" method="POST" >
+<form action="published" method="POST" class=" text-center">
     @csrf
     <input type="text" name="confession" placeholder="whats on your mind.....">
     <button type="submit">publish</button>
 </form>
+
+
+@stop

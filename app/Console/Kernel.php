3232a71353b122ac45642a30b554ca461b9c48d5
austin-sanga <austinsanga2000@gmail.com>
2022-07-26 01:41:49 +0300

    d "

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -17,7 +17,7 @@ class Kernel extends ConsoleKernel
     {
         // $schedule->command('inspire')->hourly();
         $schedule->command('day:delete')
-        ->everyMinute();
+        ->daily();
     }
 
     /**
